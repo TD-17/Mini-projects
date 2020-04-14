4194646e8bb6dc869853c6a524536c23962933be
tanya <shanya.2018td@gmail.com>
2020-04-14 12:51:30 +0530

    initialisation of snake_&_ladder

---
+++ Snake_&_ladder_game.cpp
@@ -0,0 +1,20 @@
+//Two players can play this game and the player who cross 100 first is the winner.
+#include<iostream>
+
+
+
+
+using namespace std;
+class snakeLadder
+{
+	int n,score,p1,p2;
+	char ch,name1[50],name2[50],ch;
+	public:
+		void draw_line(int n, char ch);
+		void board();
+		void gamescore(char name1[],char name2[],int p1,int p2)
+		void play_dice(int &score);
+};
+
+
+
