723b82f2e2e97fdf260a8b68c334669a07d05274
tanya <shanya.2018td@gmail.com>
2020-04-14 13:13:54 +0530

    added function board

--- Snake_&_ladder_game.cpp
+++ Snake_&_ladder_game.cpp
@@ -16,5 +16,26 @@ class snakeLadder
 		void play_dice(int &score);
 };
 
-
+void snakeLadder::draw_line(int n,char ch)
+{
+	for(int i=0;i<n;i++)
+	{
+		cout<<ch;
+	}
+	
+}
+void snakeLadder::board()
+{
+	cout<<"\n\n";
+	draw_line(50,'-');
+	cout<<"\n\t\tSNAKE AT POSITION\n";
+	draw_line(50,'-');
+	cout<<"\n\tFrom 98 to 28 \n\tFrom 95 to 24\n\tFrom 92 to 51\n\tFrom 83 to 19\n\tFrom 73 to 1\n\tFrom 69 to 33\n\tFrom 64 to 36\n\tFrom 59 to 17\n\tFrom 55 to 7\n\tFrom 52 to 11\n\tFrom 48 to 9\n\tFrom 46 to 5\n\tFrom 44 to 22\n\n";
+	drwa_line(50,'-');
+	cout<<"\n\t\t LADDER AT POSITION\n";
+	draw_line(50,'-');
+	cout<<"\n\tFrom 8 to 26\n\tFrom 21 to 82\n\tFrom 43 to 77\n\tFrom 50 to 91\n\tFrom 62 to 96\n\tFrom 66 to 87\n\tFrom 80 to 100\n";
+	draw_line(50,'-');
+	cout<<endl;
+} 
 
