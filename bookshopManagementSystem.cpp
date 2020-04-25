e1107b8638c743d4ac2741e5cff5ae1dcacda7d2
tanya <shanya.2018td@gmail.com>
2020-04-25 16:11:50 +0530

    initialisation of project bookShopManagement

---
+++ bookshopManagementSystem.cpp
@@ -0,0 +1,23 @@
+//this program is compiled in dev c++
+//before compiling it select TDM-GCC 4.9.2 32-bit Release
+#include<iostream>
+#include<fstream>
+#include<graphics.h>
+#include<iomanip>
+using namespace std;
+
+class book
+{
+char name[20];
+char author[20];
+int a;
+public:
+void getdata();
+void show();
+};
+
+int main()
+{
+	cout<<"hi there";
+}
+
