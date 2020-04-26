ee6a8352a41513a6da923581b732d067b9116cfe
tanya <shanya.2018td@gmail.com>
2020-04-26 15:01:48 +0530

    created smiley

--- bookshopManagementSystem.cpp
+++ bookshopManagementSystem.cpp
@@ -26,15 +26,44 @@ void book::getdata()
  cin>>a;
 }
 void book::show()
-{
- 
+{ 
 cout<<"BOOK    :"<<name<<endl;
 cout<<"AUTHOR  :"<<author<<endl;
 cout<<"COPIES  :"<<a<<endl;
 }
 
+void graph()
+{
+	int x,y;
+	x=getmaxx()/2;
+	y=getmaxy()/2;
+	setcolor(4);
+	circle(x,y,80);
+	setfillstyle(SOLID_FILL,YELLOW);
+	floodfill(x,y,4);
+	setfillstyle(SOLID_FILL,BLACK);
+	fillellipse(x-30,y-35,12,20);
+	fillellipse(x+30,y-35,12,20);
+	setfillstyle(SOLID_FILL,WHITE);
+	fillellipse(x-30,y-35,5,10);
+	fillellipse(x+30,y-35,5,10);
+
+	int i;
+	for(i=0;i<=35;i++)
+	{
+		ellipse(x,y,180,360,60,20+i);
+		delay(100);
+	}
+
+
+}
+
 int main()
 {
-	cout<<"hi there";
+	initwindow(800,800,"Book-Shop");
+	graph(); 
+	
+	getch();
+	return 0;
 }
 
